9b9c35b226472bf86fc9ac41a80eef2dda302c5c
FrancisLauriano <francislauriano@gmail.com>
2024-10-13 01:57:02 -0300

    ok

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,20 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from dotenv import load_dotenv
-import os
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+# from app.config.config import Config
+# from app.middlewares.cors_middleware import init_cors
 
-load_dotenv()
-
-db = SQLAlchemy()
-migrate = Migrate()
+# db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    migrate.init_app(app, db)
+    # app.config.from_object(Config)
+    # db.init_app(app)
+    # migrate = Migrate(app, db)
+    # init_cors(app)
 
-    # Import and register blueprints
-    from app.routes import usuario_routes, projeto_routes, avaliacao_routes
-    app.register_blueprint(usuario_routes.bp)
-    app.register_blueprint(projeto_routes.bp)
-    app.register_blueprint(avaliacao_routes.bp)
+    # from app.routes.usuario_routes import usuario_routes
+    # app.register_blueprint(usuario_routes)
 
     return app
+
