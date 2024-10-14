f405a3cea088ea3766cc8af1e6d0e5136260da3d
FrancisLauriano <francislauriano@gmail.com>
2024-10-14 13:27:22 -0300

    atualizado

--- app/models/usuario_model.py
+++ app/models/usuario_model.py
@@ -1,29 +1,6 @@
-from app import db
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import CheckConstraint
-import uuid
-from datetime import datetime
 
-class Usuario(db.Model):
-    __tablename__ = 'usuarios'
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    nome = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    senha = db.Column(db.String(255), nullable=False)
-    tipo = db.Column(db.String(50), nullable=False)
-    data_cadastro = db.Column(db.DateTime, default=datetime.utcnow)
 
-    __table_args__ = (
-        CheckConstraint("tipo IN ('Avaliador', 'Administrador')", name='check_tipo'),
-    )
-
-    def __init__(self, nome, email, senha, tipo):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
-        self.tipo = tipo
-
-    def __repr__(self):
-        return f'<Usuario {self.nome}, Email: {self.email}, Tipo: {self.tipo}>'
-
+# nome da classe e tabela:
+# class Usuario(db.Model):
+#     __tablename__ = 'usuarios'
