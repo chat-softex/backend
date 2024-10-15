64debbd284e9002174295fe55cd7a044b481115d
FrancisLauriano <francislauriano@gmail.com>
2024-10-15 02:19:14 -0300

    usuarios atualizado

--- app/models/usuario_model.py
+++ app/models/usuario_model.py
@@ -1,20 +1,28 @@
-# importa a instância do banco de dados da aplicação (configurada em app/__init__.py). Isso permite utilizar os recursos do SQLAlchemy.
+# Importa a instância do banco de dados da aplicação (configurada em app/__init__.py).
 from app import db
-# importa o tipo de dado UUID da extensão do PostgreSQL no SQLAlchemy, usado para gerar identificadores únicos para cada registro.
-from sqlalchemy.dialects.postgresql import UUID
-# importa a classe que permite definir restrições de verificação (check constraints) nas colunas da tabela.
-from sqlalchemy import CheckConstraint
-# biblioteca padrão de Python para gerar UUIDs, usados como identificadores únicos.
-import uuid
-# utilizado para gerar a data e hora atual no momento de cadastro dos usuários.
-from datetime import datetime
 
-# define uma classe que herda de db.Model, o que significa que ela representa uma tabela no banco de dados, neste caso, a tabela usuarios.
+# Importa o tipo de dado UUID da extensão do PostgreSQL no SQLAlchemy, usado para gerar identificadores únicos para cada registro.
+from sqlalchemy.dialects.postgresql import UUID
+
+# Importa a classe que permite definir o relacionamento com outras tabelas.
+from sqlalchemy.orm import relationship
+
+# Importa a classe que permite definir restrições de verificação (check constraints) nas colunas da tabela.
+from sqlalchemy import CheckConstraint
+
+# Biblioteca padrão de Python para gerar UUIDs, usados como identificadores únicos.
+import uuid
+
+# Utilizado para gerar a data e hora atual no momento de cadastro dos usuários.
+from sqlalchemy.sql import func
+
+
+# Define uma classe que herda de db.Model, o que significa que ela representa uma tabela no banco de dados, neste caso, a tabela usuarios.
 class Usuario(db.Model):
-    # especifica explicitamente o nome da tabela no banco de dados, que será "usuarios".
+    # Especifica explicitamente o nome da tabela no banco de dados, que será "usuarios".
     __tablename__ = 'usuarios'
 
-    # chave primária (primary_key=True) da tabela, representada por um UUID. O valor padrão (default=uuid.uuid4) gera automaticamente um UUID ao criar um novo registro.
+    # Chave primária (primary_key=True) da tabela, representada por um UUID. O valor padrão (default=uuid.uuid4) gera automaticamente um UUID ao criar um novo registro.
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
     # Coluna do tipo String com um limite de 255 caracteres. É obrigatório (nullable=False).
@@ -27,25 +35,26 @@ class Usuario(db.Model):
     senha = db.Column(db.String(255), nullable=False)
 
     # Coluna que define o tipo do usuário (ex: Avaliador ou Administrador). O valor é obrigatório (nullable=False).
-    tipo = db.Column(db.String(50), nullable=False)
+    tipo = db.Column(db.Enum('Avaliador', 'Administrador', name='usuario_tipo'), nullable=False)
 
-    # Coluna que armazena a data e hora do cadastro do usuário. O valor padrão é a data e hora atual (default=datetime.utcnow).
-    data_cadastro = db.Column(db.DateTime, default=datetime.utcnow)
+    # Coluna que armazena a data e hora do cadastro do usuário. O valor padrão é a data e hora atual (server_default=func.now()).
+    data_cadastro = db.Column(db.TIMESTAMP, server_default=func.now())
+
+    # Relacionamento com Projetos (Um-para-Muitos)
+    projetos = relationship('Projeto', back_populates='avaliador')
 
     # Define uma restrição na coluna tipo, garantindo que o valor só pode ser "Avaliador" ou "Administrador". Essa restrição garante integridade nos dados armazenados.
     __table_args__ = (
         CheckConstraint("tipo IN ('Avaliador', 'Administrador')", name='check_tipo'),
     )
 
-    #  __init__ é o construtor da classe e permite que irá inicializar um novo objeto Usuario com os valores de nome, email, senha, e tipo (apenas os dados setados pelo usuário).
+    # Construtor da classe para inicializar um novo objeto Usuario com os valores de nome, email, senha, e tipo.
     def __init__(self, nome, email, senha, tipo):
         self.nome = nome
         self.email = email
         self.senha = senha
         self.tipo = tipo
 
-
-    # __repr__ é o método que define como a instância da classe será representada quando impressa ou exibida no terminal. Ele retorna uma string que mostra o nome do usuário, o e-mail e o tipo. É util para debugar
+    # Define como a instância da classe será representada quando impressa ou exibida no terminal. Ele retorna uma string que mostra o nome do usuário, o e-mail e o tipo. É útil para debugar.
     def __repr__(self):
         return f'<Usuario {self.nome}, Email: {self.email}, Tipo: {self.tipo}>'
-
